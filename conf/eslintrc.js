9f28a5d1af9a1d40576d5c66e36feab032a4c6b1
SATO hikaru <sato-hik@gnavi.co.jp>
2019-01-22 14:44:41 +0900

    PWAtest

--- conf/eslintrc.js
+++ conf/eslintrc.js
@@ -16,10 +16,12 @@ module.exports = {
     'strict': 0,
     // console メソッドを警告
     'no-console': 1,
+    'no-unused-vars': 1,
     'no-invalid-this': 0,
     'consistent-return': 0,
     'no-magic-numbers': 0,
     'prefer-arrow-callback': 0,
+    'handle-callback-err': 0,
     //jsx記法でエラーが出ないようにする
     "import/no-named-as-default": 0,
     "import/no-named-as-default-member": 0,
